ed97b4f88ece40adf865339697a34d15748705d6
Sergio Eric <102752711+devef-com@users.noreply.github.com>
2026-07-08 15:57:01 -0500

    Update to use Deno.env.get for environment variable

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 Deno.serve((_) => {
-  const targetUrl = process.env.clan_url;
+  const targetUrl = Deno.env.get('clan_url');
   
   // Defaults to a 302 Temporary Redirect if status is omitted
   return Response.redirect(targetUrl, 301); 
