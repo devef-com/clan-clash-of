207f3f04070648d6871be37e325b7b35e9e6ccf5
Sergio Eric <102752711+devef-com@users.noreply.github.com>
2026-07-08 16:06:26 -0500

    Change redirect to return target URL
    Updated response handling to return the target URL instead of redirecting.

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 Deno.serve((_) => {
   const targetUrl = Deno.env.get('clan_url');
-  
-  // Defaults to a 302 Temporary Redirect if status is omitted
-  return Response.redirect(targetUrl, 301); 
+  const targetUrl = Deno.env.get("clan_url")!;
+  console.log("clan_url =", JSON.stringify(targetUrl));
+
+  return new Response(targetUrl);
 });
