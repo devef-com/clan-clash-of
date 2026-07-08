8461df5261051368e976f4502eb7e2eaaa9b83fc
Sergio Eric <102752711+devef-com@users.noreply.github.com>
2026-07-08 16:19:38 -0500

    Clean up Deno server code
    Removed commented-out Deno server code and unused response.

--- main.ts
+++ main.ts
@@ -1,15 +1,6 @@
-Deno.serve(() => {
-  return new Response("DEPLOY " + crypto.randomUUID(), {
-    headers: {
-      "content-type": "text/plain",
-    },
-  });
-});
-/*
 Deno.serve((_) => {
   const targetUrl = Deno.env.get('clan_url');
-  console.log("clan_url =", JSON.stringify(targetUrl));
+  //console.log("clan_url =", JSON.stringify(targetUrl));
 
   return new Response(targetUrl);
 });
-*/
