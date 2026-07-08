0740de054535230f80a29c2030984b0492943ac8
Sergio Eric <102752711+devef-com@users.noreply.github.com>
2026-07-08 16:11:40 -0500

    temporal log

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+Deno.serve(() => {
+  return new Response("DEPLOY " + crypto.randomUUID(), {
+    headers: {
+      "content-type": "text/plain",
+    },
+  });
+});
+/*
 Deno.serve((_) => {
   const targetUrl = Deno.env.get('clan_url');
   console.log("clan_url =", JSON.stringify(targetUrl));
 
   return new Response(targetUrl);
 });
+*/
