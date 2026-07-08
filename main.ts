11a6e1ee0652fab6f679c7b1272e58daf448b319
Sergio Eric <102752711+devef-com@users.noreply.github.com>
2026-07-08 16:06:45 -0500

    Fix targetUrl assignment to use non-null assertion

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 Deno.serve((_) => {
   const targetUrl = Deno.env.get('clan_url');
-  const targetUrl = Deno.env.get("clan_url")!;
   console.log("clan_url =", JSON.stringify(targetUrl));
 
   return new Response(targetUrl);
